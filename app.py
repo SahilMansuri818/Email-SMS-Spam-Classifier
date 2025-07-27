0438921cb36c7ba2117fc1d2f005df1e67cfa424
SahilMansuri818 <sahilmansuri12321@gmail.com>
2025-07-27 13:12:45 +0530

    s

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
+# Step 1: NLTK setup
+import os
+import nltk
+
+nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_dir)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_dir)
+
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab', download_dir=nltk_data_dir)
+
+
 import streamlit as st
 import pickle
 import string
